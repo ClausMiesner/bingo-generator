b58134d3c8657135895d82152d34b72887d939b7
clausmiesner <claus.miesner@web.de>
2022-02-21 17:29:28 +0100

    [#25] Implement test multiple term

--- src/test/java/de/miesner/claus/bingo/io/TermFileReaderTest.java
+++ src/test/java/de/miesner/claus/bingo/io/TermFileReaderTest.java
@@ -3,12 +3,14 @@ package de.miesner.claus.bingo.io;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TermFileReaderTest {
   TermFileReader termFileReader;
+  String pathToResources = "src/test/resources/";
 
   @Test
   void testFileDoesNotExist() {
@@ -18,9 +20,17 @@ class TermFileReaderTest {
 
   @Test
   void testOneTermInFile() throws FileNotFoundException {
-    termFileReader = new TermFileReader("src/test/resources/oneTerm");
+    termFileReader = new TermFileReader(pathToResources + "oneTerm");
     String expected = "firstTerm";
 
     assertThat(termFileReader.read()).containsExactly(expected);
+  }
+
+  @Test
+  void testMultipleTerms() throws FileNotFoundException {
+    termFileReader = new TermFileReader(pathToResources + "multipleTerms");
+    List<String> expected = List.of("firstTerm", "secondTerm", "thirdTerm");
+
+    assertThat(termFileReader.read()).containsExactlyElementsOf(expected);
   }
 }
