e393fea3e578df417d53779c758b51575c8da682
clausmiesner <claus.miesner@web.de>
2022-03-14 21:03:59 +0100

    [#33] introduce CellFormatter and require values
    For the truncation of the text to fit the cells it is important to know how many chars there can be in a row and how many rows there can be in a cell. Subsequently, the number of chars in a cell can be calculated.

---
+++ src/main/java/de/miesner/claus/bingo/util/latex/CellFormatter.java
@@ -0,0 +1,14 @@
+package de.miesner.claus.bingo.util.latex;
+
+public class CellFormatter {
+
+  private final int charsPerRow;
+
+  private final int rowsPerCell;
+
+
+  public CellFormatter(int charsPerRow, int rowsPerCell) {
+    this.charsPerRow = charsPerRow;
+    this.rowsPerCell = rowsPerCell;
+  }
+}
