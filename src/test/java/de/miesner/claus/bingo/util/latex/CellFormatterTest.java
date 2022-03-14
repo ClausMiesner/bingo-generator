3a0c087b64db2167806346f898cbfdc53be71336
clausmiesner <claus.miesner@web.de>
2022-03-14 21:16:56 +0100

    [#33] test simple short word is not modified

--- src/test/java/de/miesner/claus/bingo/util/latex/CellFormatterTest.java
+++ src/test/java/de/miesner/claus/bingo/util/latex/CellFormatterTest.java
@@ -1,6 +1,11 @@
 package de.miesner.claus.bingo.util.latex;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static de.miesner.claus.bingo.util.latex.LatexExpression.MAX_CHARS_PER_ROW;
+import static de.miesner.claus.bingo.util.latex.LatexExpression.MAX_ROWS_PER_CELL;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CellFormatterTest {
 
@@ -8,6 +13,15 @@ class CellFormatterTest {
 
   @BeforeEach
   void setUp() {
-    this.cellFormatter = new CellFormatter(0, 0);
+    this.cellFormatter = new CellFormatter(MAX_CHARS_PER_ROW, MAX_ROWS_PER_CELL);
+  }
+
+  @Test
+  @SuppressWarnings("UnnecessaryLocalVariable")
+  void testFormatToFitCell() {
+    String testString = "word";
+    String expected = testString;
+
+    assertThat(cellFormatter.formatToFitCell(testString)).isEqualTo(expected);
   }
 }
